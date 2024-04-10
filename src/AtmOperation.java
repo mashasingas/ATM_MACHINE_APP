620574957bd413e6730864b54de48c99634d8608
mashasingas <149058000+mashasingas@users.noreply.github.com>
2024-04-11 00:16:09 +0200

    Update AtmOperation.java

--- src/AtmOperation.java
+++ src/AtmOperation.java
@@ -3,6 +3,6 @@ public interface AtmOperation {
 	public void viewBalance();
 	public void  withdrawAmount(double withdrawAmount);
 	public void depositAmount(double depositAmount);
-	public void viewMinStatement();
+	public void viewMinStatement()
 
 }
